24e8a426e9cad765a52a57cf3c0f08e845f4da0c
Ryan <ryanfang11@gmail.com>
2022-06-14 13:42:09 -0700

    feat: add permission enum

--- virtool/users/utils.py
+++ virtool/users/utils.py
@@ -2,6 +2,8 @@ import hashlib
 
 import bcrypt
 
+from enum import Enum
+
 #: A list of the permission strings used by Virtool.
 PERMISSIONS = [
     "cancel_job",
@@ -13,6 +15,17 @@ PERMISSIONS = [
     "remove_job",
     "upload_file",
 ]
+
+
+class Permissions(Enum):
+    cancel_job = "cancel_job"
+    create_ref = "create_ref",
+    create_sample = "create_sample",
+    modify_hmm = "modify_hmm",
+    modify_subtraction = "modify_subtraction",
+    remove_file = "remove_file",
+    remove_job = "remove_job",
+    upload_file = "upload_file",
 
 
 def check_legacy_password(password: str, salt: str, hashed: str) -> bool:
